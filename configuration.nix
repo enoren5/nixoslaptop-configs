6e51dd3bd49acfed27e14ec65ade737a178be7cc
Joseph  Newton <drone4four@gmail.com>
2025-02-05 16:31:24 -0500

    Revert systemstate back to proper 24.11

--- configuration.nix
+++ configuration.nix
@@ -237,6 +237,6 @@ fonts.packages = with pkgs; [
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "unstable"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
