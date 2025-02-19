883964f621a910d3203211494b0a6e323c723003
Joseph  Newton <drone4four@gmail.com>
2025-02-18 20:28:49 -0500

    let in outside of pkg braces

--- configuration.nix
+++ configuration.nix
@@ -201,7 +201,8 @@
   swayidle
   hypridle
 
-    
+  ];
+
   let
      pkgs = import (builtins.fetchTarball {
          url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
@@ -209,8 +210,6 @@
   
     myPkg = pkgs.hyprland;
   in
-
-  ];
 
 
 fonts.packages = with pkgs; [
