90bdc71f1fe74ae274c5724e0a9facdcff9d80c8
Joseph Newton <drone4four@gmail.com>
2025-07-27 08:00:54 -0400

    Add pciutils + amdgpu driver (redundant?)

--- configuration.nix
+++ configuration.nix
@@ -61,10 +61,10 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
-  
   services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
@@ -154,6 +154,7 @@
   vim  
   wl-clipboard
   xclip
+  pciutils
   neovim-gtk
   wget
   gotop
