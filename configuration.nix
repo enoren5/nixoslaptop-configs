3b5e3e48c2d5886f9f75cb4d0e1be1fb63e66ae3
Joseph Newton <drone4four@gmail.com>
2025-07-29 18:19:51 -0400

    Add bleeding edge kernel versioning support

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,7 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixoslaptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -183,6 +184,7 @@
   inxi
   killall
   gawk
+  mc
 
   # Terminals
   gnome-terminal
@@ -272,8 +274,9 @@
   recoll
   clamav
   kapitano
-
-  # More fonts
+  # thunar
+  
+    # More fonts
   jetbrains-mono
   iosevka  
   nerd-fonts.symbols-only
